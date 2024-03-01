13cfc0f21b05a4b0496c189be537401c4304d326
Fantasy <37631123+Fantasy-programming@users.noreply.github.com>
2024-03-01 20:14:55 +0000

    Feat: Open images & add metadata

--- src/components/WindowDecoration.tsx
+++ src/components/WindowDecoration.tsx
@@ -6,24 +6,24 @@ const WindowDecoration = () => {
   return (
     <div
       data-tauri-drag-region
-      class=" h-10  bg-transparent shadow-subtleBtn flex justify-end top-0 left-0 right-0 fixed select-none"
+      class=" h-8 rounded-t-md bg-transparent shadow-subtleBtn flex justify-end top-0 left-0 right-0 fixed select-none"
     >
       <div
-        class="inline-flex justify-center items-center h-10 w-10 hover:bg-foreground/10"
+        class="inline-flex justify-center items-center h-8 w-8 hover:bg-foreground/10"
         id="titlebar-minimize"
         onClick={() => appWindow.minimize()}
       >
         <VsChromeMinimize class="w-1/2 h-1/2 " />
       </div>
       <div
-        class="inline-flex justify-center items-center h-10 w-10 hover:bg-foreground/10"
+        class="inline-flex justify-center items-center h-8 w-8 hover:bg-foreground/10"
         id="titlebar-maximize"
         onClick={() => appWindow.toggleMaximize()}
       >
         <VsChromeMaximize class="w-1/2 h-1/2 " />
       </div>
       <div
-        class="inline-flex justify-center items-center text-foreground hover:text-white hover:bg-destructive h-10 w-10"
+        class="inline-flex justify-center items-center text-foreground hover:text-white hover:bg-destructive h-8 w-8"
         id="titlebar-close"
         onClick={() => appWindow.close()}
       >
