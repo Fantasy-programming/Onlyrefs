4275f3c4bc0e1623b0f1e3d81319d40f786b8f69
Fantasy <37631123+Fantasy-programming@users.noreply.github.com>
2024-03-07 18:27:20 +0000

    Fix: Possible macos fix

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,6 +1,7 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 #![allow(unused_imports)]
+use tauri::Manager;
 use window_shadows::set_shadow;
 
 mod commands;
