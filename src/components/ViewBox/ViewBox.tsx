dce3d24faaa311583156d967664c030624ea291c
Fantasy <37631123+Fantasy-programming@users.noreply.github.com>
2024-03-07 22:42:57 +0000

    Feat: Add colors to view and ability to copy them

--- src/components/ViewBox/ViewBox.tsx
+++ src/components/ViewBox/ViewBox.tsx
@@ -1,6 +1,8 @@
-import { Component, ParentProps } from "solid-js";
+import { writeText } from "@tauri-apps/api/clipboard";
+import { Component, For, ParentProps } from "solid-js";
 import { Dialog, DialogContent, DialogTrigger } from "../ui/dialog";
 import { MediaRef } from "../../lib/types";
+import { Tooltip, TooltipContent, TooltipTrigger } from "../ui/tooltip";
 
 export const ViewBox: Component<ParentProps & { source: MediaRef }> = ({
   children,
@@ -9,7 +11,12 @@ export const ViewBox: Component<ParentProps & { source: MediaRef }> = ({
   return (
     <Dialog>
       <DialogTrigger class="w-full">{children}</DialogTrigger>
-      <DialogContent class="w-[90%] p-0 h-[90%] max-w-none max-h-none flex">
+      <DialogContent
+        class={"w-[90%] p-0 h-[90%] max-w-none max-h-none flex"}
+        style={{
+          background: source.metadata.colors[0],
+        }}
+      >
         <div
           class="relative flex py-2 pl-2 flex-grow justify-center items-center"
           style={{
@@ -28,10 +35,32 @@ export const ViewBox: Component<ParentProps & { source: MediaRef }> = ({
               }}
             />
           </div>
-          <div class="colors absolute"></div>
+          <div class="flex flex-row-reverse absolute bottom-4 left-4 -space-x-7 space-x-reverse  hover:space-x-0 focus-within:space-x-0   ">
+            <For each={source.metadata.colors}>
+              {(color, index) => (
+                <>
+                  <Tooltip flip={true} placement="top" openDelay={150}>
+                    <TooltipTrigger
+                      class="transition-all delay-200"
+                      onClick={async () => await writeText(color)}
+                    >
+                      <div
+                        class="w-10 h-10 rounded-full"
+                        classList={{
+                          "border-white border": index() === 0,
+                        }}
+                        style={{ background: color }}
+                      />
+                      <TooltipContent>{color}</TooltipContent>
+                    </TooltipTrigger>
+                  </Tooltip>
+                </>
+              )}
+            </For>
+          </div>
         </div>
         <div
-          class="infobox z-10  top-0 right-0 my-2 mr-2 w-[400px] absolute bg-blue-950   h-[95%] rounded-xl"
+          class="infobox z-10  top-0 right-0 my-2 mr-2 w-[400px] absolute bg-background   h-[95%] rounded-xl"
           style={{
             height: "calc(100% - (0.5rem * 2))",
           }}
@@ -52,6 +81,9 @@ export const ViewBox: Component<ParentProps & { source: MediaRef }> = ({
               <div>
                 <span>Dimension: </span>
                 <span>{source.metadata.dimensions.join(" x ")}</span>
+              </div>
+              <div>
+                <span>Date_added:</span>
               </div>
             </div>
           </div>
--- src/lib/types.ts
+++ src/lib/types.ts
@@ -6,6 +6,7 @@ export interface Metadata {
   dimensions: [number, number];
   file_size: number;
   collection: string;
+  colors: string[];
   created_at: string;
   updated_at: string;
 }
