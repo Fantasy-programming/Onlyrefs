2a1a56ab26989f3cc34c341cebf9ec7bfa6f7810
Fantasy <37631123+Fantasy-programming@users.noreply.github.com>
2024-03-11 11:48:25 +0000

    Fix: Perf improvement

--- src/lib/helper.ts
+++ src/lib/helper.ts
@@ -1,20 +1,7 @@
-import {
-  exists,
-  createDir,
-  readDir,
-  readTextFile,
-  removeDir,
-  BaseDirectory,
-  FileEntry,
-} from "@tauri-apps/api/fs";
-import { convertFileSrc, invoke } from "@tauri-apps/api/tauri";
-import { MediaRef, Metadata } from "./types";
-import {
-  COLLECTIONS_DIR,
-  breakpoints_4,
-  breakpoints_5,
-  breakpoints_6,
-} from "./config";
+import { exists, createDir, removeDir, BaseDirectory } from "@tauri-apps/api/fs";
+import { invoke } from "@tauri-apps/api/tauri";
+
+import { COLLECTIONS_DIR, breakpoints_4, breakpoints_5, breakpoints_6 } from "./config";
 
 /// Check if a ref with the given id exists
 export const refExist = async (collectionName: string) => {
@@ -39,68 +26,12 @@ export const deleteRef = async (collectionID: string) => {
   });
 };
 
-export const getAllRefs = async () => {
-  const entries = await readDir(COLLECTIONS_DIR, {
-    dir: BaseDirectory.AppData,
-    recursive: true,
-  });
-
-  const refs = entries.map((collection) => {
-    if (!collection.children) {
-      return [];
-    }
-
-    return collection.children;
-  });
-
-  return refs;
-};
-
-export const parseRefs = async (refs: FileEntry[]) => {
-  let result: MediaRef = {
-    imagepath: "",
-    low_res_imagepath: "",
-    metapath: "",
-    metadata: undefined!,
-  };
-
-  const promises = refs.map(async (ref) => {
-    if (ref.name === "metadata.json") {
-      result.metapath = ref.path;
-      result.metadata = await parseMetadata(ref.path);
-    } else if (ref.name?.startsWith("lower_")) {
-      result.low_res_imagepath = convertFileSrc(ref.path);
-    } else {
-      result.imagepath = convertFileSrc(ref.path);
-    }
-  });
-
-  await Promise.all(promises);
-
-  return result;
-};
-
-// Parse the metadata of a ref
-export const parseMetadata = async (path: string): Promise<Metadata> => {
-  const content = await readTextFile(path);
-  return JSON.parse(content);
-};
-
-export const fetchRefs = async () => {
-  const data = await getAllRefs();
-  return await Promise.all(
-    data.map(async (ref) => {
-      return await parseRefs(ref);
-    }),
-  );
-};
-
 export const changRefName = async (refID: string, newName: string) => {
   await invoke("rename_ref", { refID, newName });
 };
 
 export const deleteTag = async (refID: string, tag: string) => {
-  await invoke("delete_tag", { refID, tag });
+  await invoke("remove_tag", { refId: refID, tag: tag });
 };
 
 export const addTag = async (refID: string, tag: string) => {
